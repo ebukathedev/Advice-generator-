1b3d230bb417705be4c0a1c46b163a8c3865b526
Ebuka Ejiofor <ebuskelele@gmail.com>
2022-06-27 22:02:42 +0100

    Complete javascript code

--- main.js
+++ main.js
@@ -3,4 +3,18 @@ const adviceId = document.querySelector("#advice-id");
 const adviceString = document.querySelector("#advice-string");
 const generateAdviceBtn = document.querySelector("#generate-advice-btn");
 
+const url = "https://api.adviceslip.com/advice";
 console.log(adviceId, adviceString, generateAdviceBtn);
+
+
+generateAdviceBtn.addEventListener("click", () => {
+	fetch(url)
+		.then((res) => res.json())
+		.then((data) => {
+			adviceString.innerHTML = data.slip.advice;
+			adviceId.innerHTML = data.slip.id
+			console.log(data.slip.id, data.slip.advice);
+		});
+});
+
+window.onload = generateAdviceBtn.click();
