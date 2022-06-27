f9bd2184b7a6cacb5653c05c08519725b248af51
Ebuka Ejiofor <ebuskelele@gmail.com>
2022-06-27 09:47:13 +0100

    Add files

---
+++ main.js
@@ -0,0 +1,6 @@
+// Get elements from the DOM
+const adviceId = document.querySelector("#advice-id");
+const adviceString = document.querySelector("#advice-string");
+const generateAdviceBtn = document.querySelector("#generate-advice-btn");
+
+console.log(adviceId, adviceString, generateAdviceBtn);
